af062c301db881a274c1fa2ec0a6d92e1ba44c5d
JasonK96 <jason10101996@gmail.com>
2019-01-31 02:11:27 +0800

    Kebab detail view completed

--- app/src/main/java/com/example/asg1/DetailActivity.java
+++ app/src/main/java/com/example/asg1/DetailActivity.java
@@ -1,13 +1,62 @@
 package com.example.asg1;
 
+import android.content.Intent;
+import android.media.Rating;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
 
 public class DetailActivity extends AppCompatActivity {
+
+    TextView title, address, detailName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_layout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Bundle infoPrevious = getIntent().getExtras();
+        String cuisineName = infoPrevious.getString("cuisineName");
+        Log.d("cuisineName", cuisineName);
+
+        title = findViewById(R.id.title);
+        address = findViewById(R.id.address);
+        detailName = findViewById(R.id.detailName);
+
+        if(cuisineName.equals("kebab")){
+            title.setText(R.string.kebab);
+            detailName.setText(R.string.chunCanDetail);
+            address.setText(R.string.chunCanAddr);
+        }
+
+        if(cuisineName.equals("pizza")){
+
+        }
+
+        if(cuisineName.equals("bbq")){
+
+        }
+
+        if(cuisineName.equals("fish")){
+
+        }
     }
+
+    public void cancelAction(View view){
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        ratingBar.setRating(0);
+    }
+
+    public void saveAction(View view){
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/asg1/MainActivity.java
+++ app/src/main/java/com/example/asg1/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.asg1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+    }
+
+    public void onClick(View view){
+        Log.d("Content of img ", view.getContentDescription().toString());
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class); //intent that change to other view
+        intent.putExtra("cuisineName", view.getContentDescription().toString()); //put the extra message to next activity
+        startActivity(intent); //start change to next activity
     }
 }
